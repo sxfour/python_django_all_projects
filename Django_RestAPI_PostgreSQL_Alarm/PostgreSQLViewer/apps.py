89055d26f925beed3851526c70245264ad79c63b
Egor Levashov <levashoveg@yandex.ru>
2024-05-13 01:06:34 +0300

    Delete Django_RestAPI_PostgreSQL_Alarm directory

--- Django_RestAPI_PostgreSQL_Alarm/PostgreSQLViewer/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PostgresqlviewerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PostgreSQLViewer'
--- Django_RestAPI_PostgreSQL_Alarm/PostgreSQLViewer/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from PostgreSQLViewer.models import Users
-
-
-class UsersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Users
-        fields = '__all__'
