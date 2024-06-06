3d63f44af47a0f9afdc6aab364dc3d43b3dbaf60
Egor Levashov <levashoveg@yandex.ru>
2024-06-06 10:47:48 +0300

    Add files via upload

---
+++ Django_Docker_RestAPI_PostgreSQL_JWT_Upload/README.md
@@ -0,0 +1,13 @@
+# Django_RestAPI_PostgreSQL_JWT_Upload Server & Client (PyQT5)
+![Снимок](https://github.com/sxfour/Django_RestAPI_PostgreSQL_JWT_Upload/assets/112577182/09aaa80f-3180-4929-9ea8-4bcad472f26d)
+# Отправка HTTP запроса с клиента на сервер
+- Для авторизации клиента необходимо использовать ключ JWT
+- POST запрос загружает на сервер Django, файлы с валидацией формата .pdf/.zip
+- GET запрос для получение всего списка документов на сервере.
+  
+https://github.com/sxfour/Django_RestAPI_PostgreSQL_JWT_Upload/assets/112577182/b5daa3b3-f2e1-4a42-9af5-973151f155ed
+
+# Соединение через ssh с перенаправлением портов внутри туннеля, client <==> (SSH) remote server <==> restapi
+- При запуске сервера, в деструкторе создания маршрута, логгируется безопасная ссылка на маршрут Админ панели
+  
+https://github.com/sxfour/Django_RestAPI_PostgreSQL_JWT_Upload/assets/112577182/4931a73a-dd46-4d44-8523-62b80fd48778
---
+++ Django_Docker_RestAPI_PostgreSQL_JWT_Upload/jwt_upload_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from jwt_upload_app.serializers import DocumentsSerializer
+from jwt_upload_app.models import Documents
+
+
+# Create your views here.
+
+
+# API для отправки документов только в .pdf в POST запросе к серверу.
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def users_api(request):
+    def get_method_all():
+        data = Documents.objects.all()
+        serializer = DocumentsSerializer(data, many=True)
+
+        return Response(serializer.data)
+
+    def post_method_sent():
+        serializer = DocumentsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response({'message': 'Файл успешно отправлен'})
+
+    req_methods = {
+        'GET': get_method_all,
+        'POST': post_method_sent,
+    }
+
+    return req_methods.get(request.method)()
