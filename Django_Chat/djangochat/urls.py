44f0b02842fe72ceabf8021932a74056465720c1
Egor Levashov <levashoveg@yandex.ru>
2024-07-25 14:49:57 +0300

    Delete Django_Chat directory

--- Django_Chat/djangochat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('core.urls')),
-    path('rooms/', include('room.urls')),
-    path('admin/', admin.site.urls),
-]
--- Django_Chat/room/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.rooms, name='rooms'),
-    path('<slug:slug>/', views.room, name='room'),
-]
