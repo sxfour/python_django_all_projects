80b851f42609a338e6ceb5b32cbde4abaa46bcbe
Egor Levashov <levashoveg@yandex.ru>
2026-03-27 15:07:22 +0300

    Delete proxyshava directory

--- proxyshava/autobuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t django_proxyshava .
-docker run -it -p 8000:8000 django_proxyshava
--- proxyshava/proxyshava/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import staticfiles
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('main.urls')),
-]
